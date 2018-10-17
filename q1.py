c14e80ad2f1a2ae9622ca7770e51230cf8ba19ef
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-17 18:12:26 +0100

    q1 updated

--- q1.py
+++ q1.py
@@ -7,8 +7,7 @@ def question01(portfolios):
   # modify and then return the variable below
   answer = -1
   for i in range(len(portfolios)):
-    #for j in range(i+1, len(portfolios)):
-    for j in range(len(portfolios)):
+    for j in range(i+1, len(portfolios)):
       tmp = merge_portfolios(portfolios[i], portfolios[j])
       if tmp > answer:
         answer = tmp
@@ -20,15 +19,11 @@ def bitfield(n):
     return np.concatenate((Y,X))
 
 def merge_portfolios(p1, p2):
-  X2 = np.array(bitfield(p2))
-  C = np.array(bitfield(p1))
+  X2 = bitfield(p2)
+  C = bitfield(p1)
   for i in range(16):
     C[i] = (C[i] + X2[i]) % 2
   counter = 0
   for j in range(len(C)):
     counter += C[j] * 2 ** (15-j)
   return counter
-  
-
-# HOW TO AVOID THE 2 FOR LOOPS... ?
-#print(question01([15, 8, 6, 7, 5]))
