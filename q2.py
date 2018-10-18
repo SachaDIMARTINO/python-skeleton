1c8245142ecb5f4272dff4e5be1dba6f7abe0f0f
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-18 23:04:00 +0100

    q2 function

--- q2.py
+++ q2.py
@@ -32,3 +32,11 @@ def question02(cashflow_in, cashflow_out):
   answer = output
   return answer
 
+from itertools import chain, combinations
+def powerset(iterable):
+    """
+    powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+    """
+    xs = list(iterable)
+    # note we return an iterator rather than a list
+    return chain.from_iterable(combinations(xs,n) for n in range(len(xs)+1))
