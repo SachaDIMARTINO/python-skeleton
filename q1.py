cda31d8b25e301053e2163fce2612113b2589337
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-20 02:13:13 +0100

    q1 first optimization

--- q1.py
+++ q1.py
@@ -10,12 +10,27 @@ def question01(portfolios):
     return answer
   if len(portfolios) > 100 or max(portfolios) >= 2**16 or min(portfolios) < 0:
     return answer
-  for i in range(len(portfolios)-1):
-    for j in range(i+1, len(portfolios)):
-      X1 = bitfield(portfolios[i])
-      X2 = bitfield(portfolios[j])
-      C = [X1[k] ^ X2[k] for k in range(16)]
-      answer = max(answer, intfield(C))
+  tri = sorted(portfolios, reverse = True)
+  maxElt = max(tri)
+  maxList = []
+  minList = []
+  try:
+      indBitFort = bitfield(maxElt).index(1)
+  # que des 0 dans bitfield(maxElt) --> que des 0 dans portfolios --> return 0
+  except:
+      answer = 0
+      return answer
+  for elt in tri:
+      if 1 in bitfield(elt) and bitfield(elt).index(1) == indBitFort:
+          maxList.append(elt)
+      else:
+          minList.append(elt)
+  for n1 in maxList:
+      for n2 in minList:
+          X1 = bitfield(n1)
+          X2 = bitfield(n2)
+          C = [X1[k] ^ X2[k] for k in range(16)]
+          answer = max(answer, intfield(C))
   return answer
 
 # bitfield(15) = [0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1]
