c8f39d71156f268a8bd4ddb7897327d9ac45a6a6
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-21 14:18:30 +0100

    q4

--- q4.py
+++ q4.py
@@ -5,16 +5,26 @@ import numpy as np
 #compute all combinations for two portfolios
 def question04(rows, numberMachines):
   # modify and then return the variable below
-  answer = 20001
+  # generic cases
+  if len(rows) == 0 or numberMachines <= 0:
+    answer = 0
+    return answer
+  # constraints
+  if numberMachines > 100 or len(rows) > 100:
+    answer = 0
+    return answer
+  arrInt = [[elt if type(elt) == int else 0 for elt in arr[i]] for i in range(len(arr))]
+  infinity = max(map(max, arrInt))
+  answer = infinity
   for row in rows:
     # window
     for start in range(len(row) - numberMachines + 1):
       end = start + numberMachines
       shortRow = row[start:end]
+      print(row, start, end, shortRow)
       # si que des int dans shortRow
       if all(isinstance(x, int) for x in shortRow):
-        if sum(shortRow) < answer:
-          answer = sum(shortRow)
-  if answer == 20001:
+        answer = min(answer, sum(shortRow))
+  if answer == infinity:
     answer = 0
   return answer
