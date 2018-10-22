03c00cc26116b65e740b81a41abe9cbd264ebd0a
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-22 14:25:51 +0100

    q5 update

--- q5.py
+++ q5.py
@@ -4,11 +4,11 @@ import numpy as np
 
 def question05(allowedAllocations, totalValue):
   # modify and then return the variable below
-  answer = -1
+  answer = 0
   X = sorted(allowedAllocations, reverse = True)
   for i in range(len(X)):
     res = [X[i]]
-    while sum(res) < totalValue and i < len(X) and (len(res) < answer or answer < 0):
+    while sum(res) < totalValue and i < len(X) and (len(res) < answer or answer < 1):
       if X[i] > 0:
         if totalValue % X[i] == 0:
           if sum(res) + X[i] <= totalValue:
@@ -21,11 +21,6 @@ def question05(allowedAllocations, totalValue):
           i += 1
       else:
         i += 1
-    if sum(res) == totalValue and (len(res) < answer or answer < 0):
+    if sum(res) == totalValue and (len(res) < answer or answer < 1):
       answer = len(res)
   return answer
-
-# ERROR: Timed out. test must be < 1 sec
-# ERROR: 31/100 are incorrect. Then I changed answer = -1 instead of 20001
-# last update: if len(res) >= answer it is not necessary to continue the while
-# loop
