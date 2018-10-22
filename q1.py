bf6cb549be9fd03a77fd5302819cd0f1cbdb5f9f
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-22 01:12:48 +0100

    q1 first optimization

--- q1.py
+++ q1.py
@@ -13,9 +13,20 @@ def question01(portfolios):
   if len(portfolios) == 1:
     answer = portfolios[0]
     return answer
-  for i in range(len(portfolios)-1):
-    for j in range(i+1, len(portfolios)):
-      answer = max(answer, portfolios[i] ^ portfolios[j])
+  if max(portfolios) == 0:
+    answer = 0
+    return answer
+
+  maxElt = max(portfolios)
+  bitFort = sum([1 if maxElt-2**i >= 0 else 0 for i in range(16)]) - 1
+  maxList = [elt for elt in portfolios if elt >= 2**bitFort]
+  minList = [elt for elt in portfolios if elt not in maxList]
+  # if portfolios = [15,15,15] or [15,16,17]
+  if len(minList) == 0:
+    minList = maxList
+  for i in maxList:
+    for j in minList:
+      answer = max(answer, i ^ j)
   return answer
 
 
