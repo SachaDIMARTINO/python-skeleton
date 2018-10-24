7efecb1136948385979c8fb96072e18bc7b002e9
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-24 11:11:01 +0100

    q1 update

--- q1.py
+++ q1.py
@@ -4,29 +4,8 @@
 def question01(portfolios):
   # modify and then return the variable below
   answer = 0
-  if len(portfolios) < 2 or max(portfolios) == 0:
-    return answer
-  answer = max([portfolios[i] ^ portfolios[j] for i in range(len(portfolios)-1) for j in range(i+1, len(portfolios))])
+  for i in range(32)[::-1]:
+    answer <<= 1
+    prefixes = {num >> i for num in portfolios}
+    answer += any(answer^1 ^ p in prefixes for p in prefixes)
   return answer
-
-"""
-def question01(portfolios):
-  # modify and then return the variable below
-  portfolios = list(portfolios)
-  answer = -1
-  if len(portfolios) < 2 or max(portfolios) == 0:
-    answer = 0
-    return answer
-
-  maxElt = max(portfolios)
-  bitFort = sum([1 if maxElt-2**i >= 0 else 0 for i in range(16)]) - 1
-  maxList = [elt for elt in portfolios if elt >= 2**bitFort]
-  minList = [elt for elt in portfolios if elt not in maxList]
-  # if portfolios = [15,15,15] or [15,16,17]
-  if len(minList) == 0:
-    minList = maxList
-  for i in maxList:
-    for j in minList:
-      answer = max(answer, i ^ j)
-  return answer
-"""
