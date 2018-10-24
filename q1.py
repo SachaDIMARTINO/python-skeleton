2a3c189c48d9e0af302dd9b66922ca6faf54872a
DIMARTINO Sacha <sacha.dimartino@hotmail.fr>
2018-10-24 11:14:35 +0100

    q1 16

--- q1.py
+++ q1.py
@@ -4,7 +4,7 @@
 def question01(portfolios):
   # modify and then return the variable below
   answer = 0
-  for i in range(32)[::-1]:
+  for i in range(16)[::-1]:
     answer <<= 1
     prefixes = {num >> i for num in portfolios}
     answer += any(answer^1 ^ p in prefixes for p in prefixes)
